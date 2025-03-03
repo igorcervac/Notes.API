b5f80b00bebff2998c9a626b8abde891bd3f42aa
igorcervac <igor.cervac@outlook.com>
2025-03-03 09:08:53 -0500

    Add persisitent and in-memory note repository

---
+++ Notes.API/Models/INoteRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.API.Models
+{
+    public interface INoteRepository
+    {
+        IEnumerable<Note> GetAll();
+        Task<Note> GetByIdAsync(int id);
+        Task<Note> AddAsync(Note note);
+        Task UpdateAsync(Note entity);
+        Task DeleteAsync(Note entity);
+    }
+}
